04775bc48ad181fc18344017894cdfae284c9eaf
makeworld <colecmac@protonmail.com>
2020-08-16 11:20:08 -0400

    Remove var

--- spec/schema.rb
+++ spec/schema.rb
@@ -1,7 +1,7 @@
 require 'json-schema'
 
 describe 'nodeList.json' do
-  it "conforms to v#{schema_version}/schema.json" do
+  it "conforms to schema.json" do
     schema_file = File.expand_path("../schema/schema.json", File.dirname(__FILE__))
     nodelist_file = File.expand_path('../nodeList.json', File.dirname(__FILE__))
     JSON::Validator.validate!(schema_file, nodelist_file)
